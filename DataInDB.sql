24b1603ed703e2c86c9c15774f2b472fccbc284a
imbalanced616 <98541073+imbalanced616@users.noreply.github.com>
2023-05-02 21:35:25 +0300

    Add files via upload

---
+++ DataInDB.sql
@@ -0,0 +1,53 @@
+USE AccountingOfArrival;
+
+INSERT INTO TypesOfUsers (NameType) VALUES ('Администратор');
+INSERT INTO TypesOfUsers (NameType) VALUES ('Сотрудник');
+
+INSERT INTO Users (Login, Password, Surname, Name, Patronymic, DateOfBirthday, idUserTypes) VALUES ('osipova94@gmail.com', 'fov03*nt', 'Осипова', 'Мария', 'Романовна', '1990-10-01', 1);
+INSERT INTO Users (Login, Password, Surname, Name, Patronymic, DateOfBirthday, idUserTypes) VALUES ('bushuev95@mail.ru', '123456', 'Бушуев', 'Юрий', 'Владимирович', '1993-06-24', 2);
+
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Взгляд"', '431719328057', 'Тамбовская область, город Дмитров, проезд Ладыгина, 61');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Течение"', '807673948450', 'Кировская область, город Солнечногорск, пл. Ладыгина, 55');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Час"', '989371206836', 'Астраханская область, город Подольск, шоссе Гагарина, 13');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Активная группа"', '153974188078', 'Амурская область, город Мытищи, пр. Ладыгина, 01');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Энергия"', '792624414690', 'Ульяновская область, город Раменское, шоссе Гагарина, 94');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Специалист"', '452242119724', 'Амурская область, город Пушкино, бульвар Ломоносова, 14');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Порядок"', '846813797302', 'Читинская область, город Солнечногорск, шоссе Чехова, 03');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Вид"', '596930143177', 'Брянская область, город Луховицы, проезд Славы, 46');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Истинная способность"', '703945481164', 'Белгородская область, город Ногинск, шоссе Славы, 28');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Чудо"', '799633256475', 'Липецкая область, город Наро-Фоминск, шоссе Космонавтов, 75');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Пространство"', '115559241355', 'Ленинградская область, город Дмитров, наб. Косиора, 47');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ПАО "Субъект"', '920313630833', 'Свердловская область, город Видное, пл. 1905 года, 39');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Центр"', '774909561501', 'Новгородская область, город Ногинск, въезд Ломоносова, 91');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('АО "Земля"', '228382719439', 'Липецкая область, город Шатура, пер. Гоголя, 92');
+INSERT INTO Counterparties (Name, INN, Address) VALUES ('ООО "Система"', '726862183275', 'Саратовская область, город Лотошино, спуск Будапештсткая, 25');
+
+INSERT INTO Warehouses(Name, Address) VALUES ('Склад №1', 'Ленинградская область, город Ступино, въезд Чехова, 93');
+INSERT INTO Warehouses(Name, Address) VALUES ('Склад №2', 'Астраханская область, город Видное, спуск Ладыгина, 06');
+INSERT INTO Warehouses(Name, Address) VALUES ('Склад №3', 'Ульяновская область, город Москва, пер. Бухарестская, 61');
+
+INSERT INTO TypesOfMaterials (Name) VALUES ('Редукторы');
+INSERT INTO TypesOfMaterials (Name) VALUES ('Вентили');
+INSERT INTO TypesOfMaterials (Name) VALUES ('Защитные шлема');
+
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Корпус алюминевый', 1, 550);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Зубчатые колеса', 1, 4350);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Вал-шестерни', 1, 3100);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Валы', 1, 640);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Подшипники', 1, 237);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Крышки подшипников', 1, 549);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Прокладки', 1, 425);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Манжетные уплотнители', 1, 279);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Маховик', 2, 4037);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Втулка', 2, 35);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Набивка сальника', 2, 203);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Корпус головки алюминевый', 2, 529);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Резьба штока', 2, 1700);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Изоляция', 2, 338);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Клапан', 2, 1100);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Защитный каркас из алюминиевого сплава', 3, 21850);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Арамидная подслойка', 3, 450);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Пулестойкий забрал', 3, 15690);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Бармица', 3, 2700);
+INSERT INTO Materials (Name, idTypeOfMaterial, PricePerUnit) VALUES ('Тканевая подвесная система', 3, 14900);
+
